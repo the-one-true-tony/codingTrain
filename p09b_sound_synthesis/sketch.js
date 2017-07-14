177d087c8a01ed52cb7a4d274aa062843ed3e13c
Tony Wang <sspy45@gmail.com>
2017-07-13 21:43:38 -0700

    changed amp for synth so I could remove stop()

--- p09b_sound_synthesis/sketch.js
+++ p09b_sound_synthesis/sketch.js
@@ -1,14 +1,33 @@
 var wave;
-
+var button;
+var slider;
+var playing = false;
+let freq;
 function setup(){
   createCanvas(200,200);
-  wave = p5.Oscillator();
+  button = createButton("play/pause")
+  slider = createSlider(0, 1, 0, 0.01)
+  button.mousePressed(toggle);
+  wave = new p5.Oscillator();
   wave.setType('sine');
-  wave.amp(1);
-  wave.freq(300);
-  wave.start();
+}
+
+function toggle(){
+  if (!playing){
+    wave.start();
+    wave.amp(0.5,1);
+    playing = true;
+  } else {
+    wave.amp(0,1)
+    // wave.stop();
+    playing = false;
+  }
 }
 
 function draw(){
-  background(51);
+  playing ? background(255, 0, 255) : background(51);
+  freq = map(slider.value(), 0, 1, 100, 500);
+  // wave.amp(1);
+  wave.freq(freq);
+
 }

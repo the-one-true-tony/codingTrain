c68c4607c91442c88015d5a3c5be19b2c88210e7
Tony Wang <sspy45@gmail.com>
2017-07-13 20:53:14 -0700

    added elipse

--- p09_sound_visualization/sketch.js
+++ p09_sound_visualization/sketch.js
@@ -2,16 +2,22 @@ var song;
 var button;
 var sliderPan;
 var sliderRate;
+var amp;
 
 function preload(){
 }
 function setup(){
   createCanvas(200,200);
   song = loadSound('hey.mp3', loaded);
-  button = createButton("pause");
+  amp = new p5.Amplitude();
   sliderPan = createSlider(0, 1, 0.5, 0.01);
   sliderRate = createSlider(0, 2, 1, 0.01);
+}
+
+function loaded() {
+  button = createButton("pause");
   button.mousePressed(togglePlaying);
+  song.play();
 }
 
 function togglePlaying() {
@@ -24,12 +30,12 @@ function togglePlaying() {
   }
 }
 
-function loaded() {
-  song.play();
-}
 
 function draw(){
-  background(random(), random(), random());
+  background(51);
+
+  fill(255, 0, 255);
+  ellipse(width/2, height/2, 100, 100)
   song.setVolume(sliderPan.value());
   song.rate(sliderRate.value());
 }

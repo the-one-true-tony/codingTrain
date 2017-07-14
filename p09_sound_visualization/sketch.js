5f0e0a0333df72553578de142b39c3071915b64a
Tony Wang <sspy45@gmail.com>
2017-07-13 20:21:39 -0700

    added pause button

--- p09_sound_visualization/sketch.js
+++ p09_sound_visualization/sketch.js
@@ -1,4 +1,5 @@
 var song;
+var button;
 var sliderPan;
 var sliderRate;
 
@@ -7,8 +8,20 @@ function preload(){
 function setup(){
   createCanvas(200,200);
   song = loadSound('hey.mp3', loaded);
+  button = createButton("pause");
   sliderPan = createSlider(0, 1, 0.5, 0.01);
   sliderRate = createSlider(0, 2, 1, 0.01);
+  button.mousePressed(togglePlaying);
+}
+
+function togglePlaying() {
+  if(!song.isPlaying()){
+    song.play();
+    button.html("pause")
+  } else {
+    button.html("play")
+    song.pause();
+  }
 }
 
 function loaded() {
@@ -16,7 +29,7 @@ function loaded() {
 }
 
 function draw(){
-  background(0);
+  background(random(), random(), random());
   song.setVolume(sliderPan.value());
   song.rate(sliderRate.value());
 }

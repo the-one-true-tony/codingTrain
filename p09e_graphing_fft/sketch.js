06e320c60f62d41d764feafbb99bd34c337228b3
Tony Wang <sspy45@gmail.com>
2017-07-17 17:12:11 -0700

    added some ome additional visualization

--- p09e_graphing_fft/sketch.js
+++ p09e_graphing_fft/sketch.js
@@ -3,7 +3,7 @@ var w;
 var volHistory = [];
 
 function preload(){
-  song = loadSound('hey.mp3')
+  song = loadSound('Illenium - Fractures Trivecta Remix.mp3')
 }
 
 function toggleSong(){
@@ -21,26 +21,44 @@ function setup(){
   button = createButton('toggle');
   button.mousePressed(toggleSong);
   song.play();
-  fft = new p5.FFT(0.9, 256);
+  fft = new p5.FFT(0.8, 256);
   w = width /64;
+}
+
+function spreadSpectrum(n){
+  let newSpec = new Array(n.length);
+  let direction = true;
+  for (var i = 0; i < n.length; i++) {
+    if(direction){
+      newSpec[i] = n[i];
+    } else{
+      newSpec[n.length-(1+i/2)] = n[i];
+    }
+    direction = !direction;
+  }
+  return newSpec;
 }
 
 function draw(){
   background(0);
   let spectrum = fft.analyze();
+  spectrum = spreadSpectrum(spectrum);
   // console.log(spectrum);
-  noStroke();
+  // noStroke();
   translate(width/2, height/2);
   beginShape();
   for (var i = 0; i < spectrum.length; i++) {
     let angle = map(i, 0, spectrum.length, 0, 360);
     let amp = spectrum[i];
-    let r = map(amp, 0, 256, 40, 100);
+    let r = map(amp, 0, 256, 60, 100);
     let x = r * cos(angle);
     let y = r * sin(angle);
+    strokeWeight(3);
+    stroke(100, 255, 255);
+    // line(0, 0, x, y);
     vertex(x,y);
     // fill(i, 255, 255);
     // rect(i*w, y, w, height -y);
   }
-  endShape();
+  endShape(CLOSE);
 }
